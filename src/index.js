b5112f6376e683269f27a8197e78fb87a974c3f0
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-05-07 14:35:19 +0200

    fix minor typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/website";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
